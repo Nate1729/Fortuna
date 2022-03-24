ea077a14a1ffa65e9f9c4fe2f0780442772ff6a6
Nate Ferreira <nferreira1729@gmail.com>
2022-03-24 12:23:58 -0400

    Fix linting

--- fortuna/app.py
+++ fortuna/app.py
@@ -3,21 +3,20 @@ from tkinter import ttk
 
 from frames.skat import SkatFrame
 
-class App(tk.Tk):
-    
 
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         # root window configuration
-        self.title('Fortuna (Skat)')
-        self.geometry('700x300')
-        #self.grid_rowconfigure(0, weight=1)
-        #self.grid_columnconfigure(0, weight=1)
+        self.title("Fortuna (Skat)")
+        self.geometry("700x300")
+        # self.grid_rowconfigure(0, weight=1)
+        # self.grid_columnconfigure(0, weight=1)
 
 
-if __name__ == '__main__':
-    players = ['Nate', 'Matt', 'Josh']
+if __name__ == "__main__":
+    players = ["Nate", "Matt", "Josh"]
     app = App()
     frame_skat = SkatFrame(app, players)
     frame_skat.grid(row=0, column=0)
