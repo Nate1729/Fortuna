d5957c9dfb4b2390307d0656c25b592a66071b53
Nate <nferreira1729@gmail.com>
2022-04-06 08:27:07 -0400

    Feature/issue 5/add tests to skat UI (#7)
    * Add test command for poetry
    
    * Add initial tests
    
    * Fix formatting
    
    * Fix formatting

---
+++ tests/test_skat.py
@@ -0,0 +1,44 @@
+import unittest
+import tkinter as tk
+
+from fortuna.frames import skat
+
+
+class TestHistoryFrame(unittest.TestCase):
+    def setUp(self):
+        self.root = tk.Tk()
+
+    def tearDown(self):
+        if self.root:
+            self.root.destroy()
+
+    def test_table_header(self):
+        history = skat.HistoryFrame(self.root)
+        history.grid(row=0, column=0)
+        columns = (
+            "Bidder",
+            "Bid",
+            "Game",
+            "Declared",
+            "Result",
+            "With",
+            "Scored",
+        )
+
+        self.assertTupleEqual(history.history_tree["columns"], columns)
+
+
+class TestBidFrame(unittest.TestCase):
+    def setUp(self):
+        self.root = tk.Tk()
+        self.players = ("Player 1", "Player 2", "Player 3")  # should be a tuple
+        self.test_frame = skat.BidFrame(self.root, self.players)
+
+    def tearDown(self):
+        if self.test_frame:
+            self.test_frame.destroy()
+        if self.root:
+            self.root.destroy()
+
+    def test_bidder_values(self):
+        self.assertEqual(self.test_frame.combo_player["values"], self.players)
