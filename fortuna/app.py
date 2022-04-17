8fa42502b8aa8a178d0f69cc8b1453a246622be5
Nate Ferreira <nferreira1729@gmail.com>
2022-04-16 21:20:31 -0400

    add validation

--- fortuna/app.py
+++ fortuna/app.py
@@ -10,7 +10,7 @@ class App(tk.Tk):
 
         # root window configuration
         self.title("Fortuna (Skat)")
-        self.geometry("700x300")
+        self.geometry("700x350")
         # self.grid_rowconfigure(0, weight=1)
         # self.grid_columnconfigure(0, weight=1)
 
